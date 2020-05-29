d9eb31ccbace9d6aac1f3789d37e38d220ce6a69
mollifier <mollifier@gmail.com>
2020-05-29 21:05:09 +0900

    Add help message

--- functions/cd-gitroot.fish
+++ functions/cd-gitroot.fish
@@ -3,11 +3,17 @@ function cd-gitroot
     or return 1
 
   if set -lq _flag_h
-    _cd-gitroot_help
+    _cd-gitroot_print_help
     return
   end
 end
 
-function _cd-gitroot_help
-  echo 'help message'
+function _cd-gitroot_print_help
+  echo 'Usage: cd-gitroot [PATH]
+Change directory to current git repository root directory.
+If PATH is specified, change directory to PATH instead of it.
+PATH is treated relative path in git root directory.
+
+-h, --help    display this help and exit'
+
 end
