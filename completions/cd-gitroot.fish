aa525b53c8e6ade8e4157a56512e7adbd062534e
mollifier <mollifier@gmail.com>
2020-05-31 17:35:35 +0900

    Complete directory

--- completions/cd-gitroot.fish
+++ completions/cd-gitroot.fish
@@ -1,1 +1,26 @@
-complete -c cd-gitroot -s h -l help
+complete -c cd-gitroot -x
+complete -c cd-gitroot -n '__fish_is_first_token' -s h -l help -d 'Display this help and exit'
+complete -c cd-gitroot -n '__fish_is_first_token' -xa '(_cd-gitroot_complete_path)'
+
+function _cd-gitroot_complete_path
+  if not git rev-parse --is-inside-work-tree > /dev/null 2>&1
+    # not in git working tree
+    return 2
+  end
+
+  set -l root_path (git rev-parse --show-toplevel)
+  _cd-gitroot_fish_complete_directories $root_path (commandline -tc)
+end
+
+function _cd-gitroot_fish_complete_directories
+  set -l base $argv[1]
+  set -l current $argv[2]
+
+  set -l target $base
+  if test -n "$current"
+    set target $current
+  end
+
+  command ls -dp "$target"*
+end
+
